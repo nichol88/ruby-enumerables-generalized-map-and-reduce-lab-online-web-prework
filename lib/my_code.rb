585780d272dd8a59a91b9103964a8ca2ab505847
Brian Nicholls <nichol88@gmail.com>
2019-11-06 06:27:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+def map(array)
+  result = []
+  count = 0
+  while count < array.length
+    cnum = yield(array[count])
+    result.push(cnum)
+    count += 1
+  end
+  result
+end
+
+
+def reduce(array, starting_point = 0)
+  count = 0
+  total = starting_point
+  while count < array.length
+
+    cur_num = array[count]
+
+    puts "#{count} : Current num: #{cur_num}"
+    puts "Total: #{total}"
+
+    total = yield(cur_num, total)
+
+    if !total
+      total = false
+    end
+
+    count += 1
+  end
+  total
+end
